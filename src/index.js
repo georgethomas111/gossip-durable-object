4920faae3335b636a23078bd597d99134f20383e
George Thomas <gerogethomas.mec@gmail.com>
2025-04-10 21:57:01 -0700

    Push the changes for now

--- src/index.js
+++ src/index.js
@@ -1,26 +1,3 @@
-/*
-import { DurableObject } from "cloudflare:workers";
-
-export class MyDurableObject extends DurableObject {
-	constructor(ctx, env) {
-		super(ctx, env);
-	}
-
-	async sayHello(name) {
-		return `Hello, ${name}!`;
-	}
-}
-
-export default {
-	async fetch(request, env, ctx) {
-		const id = env.MY_DURABLE_OBJECT.idFromName("foo");
-		const stub = env.MY_DURABLE_OBJECT.get(id);
-		const greeting = await stub.sayHello("world");
-		return new Response(greeting);
-	},
-};
-*/
-// index.js
 import { DurableObject } from "cloudflare:workers";
 import { Nodes } from "./nodes.js";
 
